8b07a937beff8da722f1cca51f14c0871abd99a0
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-02 19:44:36 -0500

    added search state and search input

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     super();
 
     this.state = {
-      characters: []
+      characters: [],
+      searchField: ''
     }
   }
 
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Star Wars Characters</h1>
+        <input type='search' placeholder='Search Character' onChange={e => this.setState({searchField: e.target.value}, () => console.log(this.state.searchField))}/>
         <CharacterList characters={this.state.characters}/>
       </div>
     );
