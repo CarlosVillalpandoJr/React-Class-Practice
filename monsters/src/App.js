6ff9c74bbe56888c04e61d4ae5812639c966d5f4
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-02 19:53:54 -0500

    added functional searchbox

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -23,11 +23,15 @@ class App extends Component {
 
 
   render() {
+    const { characters, searchField } = this.state
+    const filteredCharacters = characters.filter(char => 
+      char.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className='App'>
         <h1>Star Wars Characters</h1>
-        <input type='search' placeholder='Search Character' onChange={e => this.setState({searchField: e.target.value}, () => console.log(this.state.searchField))}/>
-        <CharacterList characters={this.state.characters}/>
+        <input type='search' placeholder='Search Character' onChange={e => this.setState({searchField: e.target.value})}/>
+        <CharacterList characters={filteredCharacters}/>
       </div>
     );
   }
